0f75c54ae4408c1d9ddf554c4e5d9ef9b1564e76
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-06-26 01:27:32 +0900

    Title: 변수 출력하기 3, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240626/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int a = 1;
+    static int b = 2;
+    static char c = 'C';
+    public static void main(String[] args) {
+        System.out.println(a+"->"+b+"->"+c);
+    }
+}
