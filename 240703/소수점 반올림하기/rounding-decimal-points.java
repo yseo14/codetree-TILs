d67255421696c817207c1e02343e696cbf7fb725
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-07-03 22:40:09 +0900

    Title: 소수점 반올림하기, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240703/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a = 25.352;
+        double result;
+        result = Math.round(a*10)/10.0;
+        System.out.println(result);
+        // System.out.printf("%.1f", a);
+    }
+}
