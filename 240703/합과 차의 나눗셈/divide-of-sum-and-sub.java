44e11d38e6b0da7fe7a7a8ea1ae6a78cc5062f71
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-07-03 23:48:01 +0900

    Title: 합과 차의 나눗셈, Time: 108ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240703/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        double result = (double)(a+b)/(a-b);
+        System.out.printf("%.2f",result);
+
+    }
+}
