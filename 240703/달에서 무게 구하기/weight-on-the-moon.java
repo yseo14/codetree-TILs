73b9649e311eeb5655614deedb0a38a3e1d1063b
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-07-03 22:30:44 +0900

    Title: 달에서 무게 구하기, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240703/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int weight = 13;
+        double rate = 0.165;
+
+        System.out.printf(weight+" * "+"%.6f"+" = "+ "%.6f",rate, rate*(double)weight);
+    }
+}
