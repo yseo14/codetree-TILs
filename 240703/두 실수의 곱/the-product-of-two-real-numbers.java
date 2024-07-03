685658e369bd5f450fb0003fa06c91e31fa87482
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-07-03 22:53:06 +0900

    Title: 두 실수의 곱, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240703/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a = 5.26;
+        double b = 8.27;
+
+        double result = a*b;
+        System.out.printf("%.3f", result);
+    }
+}
