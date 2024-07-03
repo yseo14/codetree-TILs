40f45022632057cbebe3e32c720e0fa23be3144e
Yoonseo Lee <101577272+yseo14@users.noreply.github.com>
2024-07-03 23:49:58 +0900

    Title: 합과 평균의 차, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240703/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(a+b+c);
+        System.out.println((a+b+c)/3);
+        System.out.println((a+b+c)-(a+b+c)/3);
+    }
+}
